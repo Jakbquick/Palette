d53a795c7392357c92e77feab8d5871a952ebcc9
John Manard III <jakbquick2000@gmail.com>
2018-05-24 20:05:17 -0700

    Added rectangle representation of world.

--- SourceFiles/desktop/src/com/fourmen/apcs/desktop/DesktopLauncher.java
+++ SourceFiles/desktop/src/com/fourmen/apcs/desktop/DesktopLauncher.java
@@ -5,7 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.fourmen.apcs.Palette;
 
 import static com.fourmen.apcs.Palette.TITLE;
-
+/**
+ *this class constitures the main class of the Palette game. Begins game with desired configuration for
+ * desktop play.
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
