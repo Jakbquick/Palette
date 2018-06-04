ea48432667786039b87a03b5e188914a88abe8b3
John Manard III <jakbquick2000@gmail.com>
2018-06-03 19:39:43 -0700

    camera starts on player

--- SourceFiles/core/src/com/fourmen/screens/Box2DRender.java
+++ SourceFiles/core/src/com/fourmen/screens/Box2DRender.java
@@ -3,14 +3,15 @@ package com.fourmen.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.fourmen.actors.Box2DEnemy;
 import com.fourmen.actors.Box2DPlayer;
@@ -24,7 +25,14 @@ import com.fourmen.utils.CameraStyles;
 public class Box2DRender extends ScreenAdapter {
     private static float WORLD_WIDTH;
     private static float WORLD_HEIGHT;
-
+    private boolean isPaused;
+    public enum State
+    {
+        PAUSE,
+        RUN,
+        RESUME,
+        STOPPED
+    }
 
     SpriteBatch batch;
     World world;
@@ -44,6 +52,8 @@ public class Box2DRender extends ScreenAdapter {
     private Viewport viewport;
     private float timeSinceStart;
 
+    Texture fadedBlack = new Texture(Gdx.files.internal("Images/FadedBlack.png"));
+    Texture pauseText = new Texture(Gdx.files.internal("Images/PauseText.png"));
 
     public Box2DRender(int width, int height){
         WORLD_WIDTH = width;
@@ -84,7 +94,7 @@ public class Box2DRender extends ScreenAdapter {
         enemy = new Box2DEnemy(world, player);
         walls = new Walls(world,0,0, scale);
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
+        camera.position.set(player.getX(), player.getY(), 0);
         camera.update();
         //viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
         viewport = new ExtendViewport(camera.viewportWidth / 2f, camera.viewportHeight / 2f, camera);
@@ -112,6 +122,7 @@ public class Box2DRender extends ScreenAdapter {
             walls.draw(batch);
             player.draw(batch);
         }
+
         batch.end();
     }
     private void debugView() {
