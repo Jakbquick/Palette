70f8141d404f4401821a24236166358a4637e15c
John Manard <johnmanardiii@gmail.com>
2018-06-04 00:34:18 -0700

    started to work on the beatmapmaker

--- Tools/BeatMapMaker/core/src/com/john/epic/MapMaker.java
+++ Tools/BeatMapMaker/core/src/com/john/epic/MapMaker.java
@@ -2,32 +2,38 @@ package com.john.epic;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MapMaker extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture redCircle;
+	private float widthCircle = 300;
+	private Music secretAgent;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		redCircle = new Texture("Images/RedCircle.png");
+		secretAgent = Gdx.audio.newMusic(Gdx.files.internal("Music/SecretAgent.mp3"));
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		secretAgent.play();
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(redCircle, 250 - (widthCircle/2f), 250 - (widthCircle/2f),
+				widthCircle,widthCircle);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		redCircle.dispose();
 	}
 }
--- Tools/BeatMapMaker/desktop/src/com/john/epic/desktop/DesktopLauncher.java
+++ Tools/BeatMapMaker/desktop/src/com/john/epic/desktop/DesktopLauncher.java
@@ -7,6 +7,13 @@ import com.john.epic.MapMaker;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "BEATMAKER";
+		config.vSyncEnabled = true;
+		config.useGL30 = true;
+		config.width = 500;
+		config.height = config.width;
+		config.useGL30 = false;			// runs without using openGl so Collin's baka computer can handle it
+		config.resizable = false;
 		new LwjglApplication(new MapMaker(), config);
 	}
 }
