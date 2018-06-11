cf1efdcc1608beb8376efc7ec44111d1e0137fe2
John Manard III <jakbquick2000@gmail.com>
2018-06-10 23:24:36 -0700

    Made game same for all screen sizes

--- SourceFiles/core/src/com/fourmen/utils/RhythmView.java
+++ SourceFiles/core/src/com/fourmen/utils/RhythmView.java
@@ -69,7 +69,7 @@ public class RhythmView {
         xDistance = Gdx.graphics.getWidth() + (.5f * Beat.beatWidth) -        //the first beatSize here is the size of the beat transitioning across the map (replace later)
                 100 - (.5f * beatWidth);
         centerWhen = new Vector2(100 + (beatWidth/2f), y + (.5f * bar.getHeight()));
-        velocity = 350f / 60f;
+        velocity = ((5.83f)*(Gdx.graphics.getWidth())) / 1920fs;
         timeBeforeSpawn = (velocity * 60f) / xDistance;
         remainder = timeBetweenNotes - (timeBeforeSpawn % timeBetweenNotes);
         timeToFillMap = timeBeforeSpawn + remainder + offset;
